9c0969236749612dec7a2e420cbe51439e81ff56
ayseozates <69298065+ayseozates@users.noreply.github.com>
2021-01-28 12:13:56 +0300

    Add files via upload

---
+++ CustomerManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void AddCustomer (List<Customer> customers)
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Müsteri Sisteme Başarıyla eklendi :" + customer.FirstName + " " + customer.LastName);
+            }
+
+           
+        }
+        public void ListCustomer(List<Customer> customers)
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Müsteriler listelendi :" + customer.FirstName + " " + customer.LastName);
+            }
+
+        }
+        public void DeleteCustomer(List<Customer> customers)
+        {
+            Console.WriteLine("Müsteri Başarıyla Sistemden Silindi");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bankada Müşteri Takibi!");
+            Customer customer1 = new Customer() {
+                FirstName = "Ayşe",
+                LastName="Özateş"
+            
+            
+            };
+            Customer customer2 = new Customer()
+            {
+                FirstName = "Fatma",
+                LastName = "özateş"
+
+
+            };
+            List<Customer> customers = new List<Customer>() { customer1, customer2 };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.AddCustomer(customers);
+            customerManager.ListCustomer(customers);
+            customerManager.DeleteCustomer(customers);
+
+        }
+    }
+}
